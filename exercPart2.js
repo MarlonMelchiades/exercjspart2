44d4f3d1c6b264b388d6b8f5867d13f4940f21d2
marlonmelchiades <79936558+MarlonMelchiades@users.noreply.github.com>
2021-10-11 16:52:42 -0300

    Add files via upload

---
+++ exercPart2.js
@@ -0,0 +1,369 @@
+// ----- exercício 21 ----- //
+
+function calcularPreco (idade) {
+    if(idade < 10){
+        return 180
+    } else if(idade < 30) {
+        return 150
+    } else if (idade < 60) {
+        return 195
+    } else {
+        return 230
+    }
+}
+
+console.log(calcularPreco(8));
+console.log(calcularPreco(15));
+console.log(calcularPreco(35));
+console.log(calcularPreco(52));
+console.log(calcularPreco(80));
+
+// ----- exercício 22 ----- //
+
+function calcularValor(mes, valor){
+    if(mes > 0 && mes < 13) {
+        atraso = mes - 1
+        return (valor * ((1 + (5/100))**atraso)).toFixed(2)
+    } else {
+        return 'Mês inválido.'
+    }
+}
+
+console.log(calcularValor(4, 100))
+
+// ----- exercício 23 ----- //
+
+function calcularNotaFinal(codAluno, nota1, nota2, nota3){
+    let notas = []
+    notas.push(nota1)
+    notas.push(nota2)
+    notas.push(nota3)
+    notas.sort((a,b) => a < b ? 1 : -1)
+
+    let mediaFinal = (notas[0] * 4 + notas[1] * 3 + notas[2] * 3)/10
+    console.log(`Código do Aluno: ${codAluno}. Notas: ${nota1}, ${nota2}, ${nota3}. ${mediaFinal < 5 ? 'Reprovado.' : 'Aprovado.'}`)
+}
+
+calcularNotaFinal(123, 2.8, 6, 3.5)
+
+// ----- exercício 24 ----- //
+
+function helloWorld () {
+   let i = 0
+    while (i < 11) {
+        console.log("Hello World");
+        i++
+    }   
+}
+
+helloWorld()
+
+// ----- exercício 25 ----- //
+
+function contador () {
+    for (let i = 1; i<=50; i++) {
+        console.log(i);
+    }
+}
+contador()
+
+// ----- exercício 26 ----- //
+
+function acharPares () {
+    for (let i = 1; i <= 100; i++){
+        if (i%2==0) {
+            console.log(i)
+        }
+    }
+}
+
+acharPares()
+
+// ----- exercício 27 ----- //
+
+function calcularCrescimento(altura1, taxa1, altura2, taxa2) {
+    if(altura1 == altura2){
+        if (taxa1 > taxa2) {
+            return 'A criança 1 ultrapassará a criança 2 em 1 ano.'
+        } else if(taxa1 < taxa2) {
+            return 'A criança 2 ultrapassará a criança 1 em 1 ano.'
+        }else{
+            return 'As crianças tem igual altura e crescimento.'
+        }
+    } else {
+        if (altura1 > altura2) {
+            if(taxa1 >= taxa2){
+                return 'A criança menor não ultrapassará a maior.'
+            }else{
+               return `A criança menor ultrapassará a maior em ${calcularTempo(altura1, taxa1, altura2, taxa2)} anos` 
+            }
+        } else {
+            if(taxa2 >= taxa1){
+                return 'A criança menor não ultrapassará a maior.'
+            }else{
+               return `A criança menor ultrapassará a maior em ${calcularTempo(altura1, taxa1, altura2, taxa2)} anos` 
+            }
+        }
+    }
+}
+
+function calcularTempo(alturaMenor, taxaAlturaMenor, alturaMaior, taxaAlturaMaior){
+    let qtdAnos = 0
+    while (alturaMenor < alturaMaior) {
+        alturaMenor += taxaAlturaMenor
+        alturaMaior += taxaAlturaMaior
+        qtdAnos++
+    }
+    //console.log(qtdAnos)
+    return qtdAnos
+}
+
+console.log(calcularCrescimento(150, 2, 130, 4));
+
+// ----- exercício 28 ----- //
+
+function paresImpares(vetorNumeros) {
+    let qtdPares = 0
+    let qtdImpares = 0
+    for (let i = 0; i < vetorNumeros.length; i++) {
+        if (vetorNumeros[i] % 2 == 0) {
+            qtdPares++
+        } else {
+            qtdImpares++
+        }
+    }
+    console.log(`${qtdPares} números pares e ${qtdImpares} números ímpares.`)
+}
+
+vetor = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+paresImpares(vetor)
+
+// ----- exercício 29 ----- //
+
+function observarIntervalo (vetor) {
+    qtdNumerosNoIntervalo = 0
+    for (let i = 0; i < vetor.length; i++) {
+        if(vetor[i] >= 10 && vetor[i] <= 20){
+            qtdNumerosNoIntervalo++
+        }
+    }
+    return `${qtdNumerosNoIntervalo} números dentro do intervalo.`
+}
+
+vetor = [ 7, 8, 9, 10, 11, 12, 16, 17, 18, 19, 20, 21]
+
+console.log(observarIntervalo(vetor))
+
+// ----- exercício 30 ----- //
+
+function maiorMenor (vetor) {
+    let maior
+    let menor
+    for (let i = 0; i < vetor.length; i++){
+        if (maior === undefined && menor === undefined){
+            maior = vetor[i]
+            menor = vetor[i]
+        } else {
+            if (vetor[i] > maior){
+                maior = vetor[i]
+            }
+            if(vetor[i] < menor){
+                menor = vetor[i]
+            }
+        }
+    }
+    return [maior, menor]
+}
+
+vetor =  [10, 5, 7, 3, 1, 3, 11, 20, 6, 9]
+
+console.log(maiorMenor(vetor))
+
+// ----- exercício 31 ----- //
+
+function numerosNegativos (vetor) {
+    let qtdNegativos = 0
+    for(let i = 0; i < vetor.length; i++) {
+        if(vetor[i] < 0) {
+            qtdNegativos++
+        }
+    }
+    return qtdNegativos
+}
+
+vetor =  [10, 5, -7, 3, -1, 3, -11, -20, 6, 9]
+
+console.log(numerosNegativos(vetor))
+
+// ----- exercício 32 ----- //
+
+function mediaVetor (vetor) {
+    let soma = 0
+    for(let i=0; i<vetor.length; i++) {
+        soma += vetor[i]
+    }
+
+    return soma/vetor.length
+}
+
+vetor = [1, 2, 3, 4, 5]
+
+console.log(mediaVetor(vetor))
+
+// ----- exercício 33 ----- //
+
+let vetorInteiro = [1, 2, 3, 4]
+let vetorString = ['Arthur', 'Christian', 'Yuri', 'Galdino']
+let vetorDouble = [1.1, 2.2, 3.3, 4.4]
+
+function concatenar (...args) {
+    resultado = []
+    for(let i = 0; i<arguments.length; i++){
+        resultado = resultado.concat(arguments[i])
+    }
+    return resultado;
+}
+
+console.log(concatenar(vetorInteiro, vetorDouble))
+console.log(concatenar(vetorDouble, vetorString))
+
+// ----- exercício 34 ----- //
+
+function verificacaoDeString (string1, string2) {
+    let estaContido = true;
+    for (let i = 0; i < string1.length; i++) {
+        let caractereString1 = string1.charAt(i).toLowerCase()
+        for(let j = 0; j < string2.length; j++){
+            let caractereString2 = string2.charAt(j).toLowerCase()
+            if(caractereString1 == caractereString2) {
+                estaContido = true
+                break
+            } else {
+                estaContido = false
+            }
+        }
+        if(!estaContido) {
+            return estaContido
+        }
+    }
+    return estaContido
+ }
+
+ console.log(verificacaoDeString('abc','cba'))
+
+// ----- exercício 35 ----- //
+
+let vetorPilha = [1, 2, 3, 4, 5]
+let vetorAdiciona = [6, 7, 8, 9, 10]
+
+function adicionaVetor(vetorInicial, vetorAdicionar) {
+    for(let i = 0; i < vetorAdicionar.length; i++) {
+        console.log(vetorAdicionar[i])
+        vetorInicial.push(vetorAdicionar[i])
+    }
+    console.log('Vetor adicionado: ' + vetorAdicionar)
+    console.log('Vetor resultado: ' + vetorInicial)
+}
+
+adicionaVetor(vetorPilha, vetorAdiciona)
+
+// ----- exercício 36 ----- //
+
+let vetor = [1, 2, 3, 4, 5]
+
+function multiplicaVetor(vetor, multiplicador){
+    let vetorResultado = []
+    vetor.forEach(elemento => {
+        vetorResultado.push(elemento * multiplicador) 
+    });
+
+    return vetorResultado
+}
+
+console.log(multiplicaVetor(vetor, 3))
+
+// ----- exercício 37 ----- //
+
+function pA (n, a1, r) {
+    for (let i = 0; i < n; i++) {
+        console.log(a1 + r*i)
+    }
+    console.log('Soma: ' + (n * (a1 + (a1 + ((n-1)*r))))/2)    
+}
+
+function pG (n, a1, r) {
+    //console.log(a1)
+    for(let i = 0; i < n; i++){
+        console.log(a1*(r**i))
+    }
+    console.log('Soma: ' + (a1 * ((r**n)-1))/(r-1))
+}
+
+pA(10, 10, 15)
+console.log('----------------');
+pG(10, 5, 3)
+
+// ----- exercício 38 ----- //
+
+function imprimirImpares(inicio = 0, fim = 100) {
+    if (inicio > fim) {
+        inicio = fim + inicio
+        fim = inicio - fim
+        inicio = inicio - fim        
+    }
+
+    for (let i = inicio; i <= fim; i++) {        
+        if (i % 2 == 1) {
+            console.log(i)
+        }
+    }
+}
+
+imprimirImpares(19, 3)
+
+// ----- exercício 39 ----- //
+
+function trocaValores(vetorA, vetorB) {
+    if (vetorA.length == vetorB.length) {
+        for(let i = 0; i < vetorA.length; i++){
+            vetorA[i] = vetorA[i] + vetorB[i]
+            vetorB[i] = vetorA[i] - vetorB[i]
+            vetorA[i] = vetorA[i] - vetorB[i]
+        }
+    } else {
+        return 'Vetores de tamanhos diferentes.'
+    }
+
+    console.log('Novo vetor A: ' + vetorA)
+    console.log('Novo vetor B: ' + vetorB)
+}
+
+let vetorA = [1, 2, 3]
+let vetorB = [4, 5, 6]
+
+trocaValores(vetorA, vetorB)
+
+// ----- exercício 40 ----- //
+
+function conceituarNotas(notas) {
+    let conceitos = []
+    for(let i = 0; i < notas.length; i++) {
+        if(notas[i]>=0 && notas[i]<=4.9){
+            conceitos.push('D')
+        } else if(notas[i]>=5 && notas[i]<=6.9) {
+            conceitos.push('C')            
+        } else if(notas[i]>=7 && notas[i]<=8.9) {
+            conceitos.push('B')
+        } else if(notas[i]>=9 && notas[i]<=10){
+            conceitos.push('A')
+        } else {
+            conceitos.push('Nota inválida')       
+       }
+    }
+    return conceitos
+}
+
+let notas = [10, 13, 9, 8.2, 2.5, 7.7, 6.8]
+let vetorConceitos = conceituarNotas(notas)
+
+console.log(vetorConceitos)
